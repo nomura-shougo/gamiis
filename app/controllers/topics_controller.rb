9871bc2173ac786df827cd0d9eabb45f0f6e274e
nomura <s.nomura475565@gmail.com>
2021-05-19 03:37:02 +0900

    グループ作成を追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,9 +1,25 @@
 class TopicsController < ApplicationController
-  def index; end
+  def index
+    @topics = Topic.all
+  end
 
   def new
     @topic = Topic.new
   end
 
-  def create; end
+  def create
+    @topic = User.new(user_params)
+    if @topic.save
+      flash[:success] = "グループの登録が完了しました"
+      redirect_to topics_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def topic_params
+      params.require(:topic).permit(:name)
+    end
 end
