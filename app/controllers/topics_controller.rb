653d78af5874c54a7d108476efa14f1c857d288b
nomura <s.nomura475565@gmail.com>
2021-05-19 04:29:50 +0900

    グループの削除とflashメッセージ追加

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,13 +8,25 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = User.new(user_params)
+    @topic = Topic.new(topic_params)
     if @topic.save
       flash[:success] = "グループの登録が完了しました"
       redirect_to topics_url
     else
       render 'new'
     end
+  end
+
+  def destroy
+    topic = Topic.find(params[:id])
+    if topic.blank?
+      flash[:danger] = 'グループが存在しません'
+    else
+      topic.destroy!
+      flash[:success] = 'グループを削除しました。'
+    end
+
+    redirect_to topics_url
   end
 
   private
