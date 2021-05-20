2108a06ee56de54be856b509aed3f0a3c968ed12
nomura <s.nomura475565@gmail.com>
2021-05-21 03:20:11 +0900

    postsテーブルのmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_180618) do
+ActiveRecord::Schema.define(version: 2021_05_20_180135) do
+
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "topic_id"
+    t.bigint "replied_to_post_id"
+    t.text "content"
+    t.integer "post_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["replied_to_post_id"], name: "index_posts_on_replied_to_post_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "topics", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -28,4 +41,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_180618) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "posts", column: "replied_to_post_id"
+  add_foreign_key "posts", "topics"
+  add_foreign_key "posts", "users"
 end
