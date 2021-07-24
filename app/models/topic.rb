a3292e4dfb2902a2b703ddc52865e24b16b9795e
nomura <s.nomura475565@gmail.com>
2021-07-25 03:30:09 +0900

    グループのソート機能を追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,15 @@ class Topic < ApplicationRecord
   has_many :users, through: :participations
 
   mount_uploader :picture, PictureUploader
+
+  ransacker :users_count do
+    query = '(SELECT COUNT(*) FROM `users` INNER JOIN `participations` ON `users`.`id` = `participations`.`user_id`
+    where participations.topic_id = topics.id  group by participations.topic_id)'
+    Arel.sql(query)
+  end
+
+  ransacker :latest_post_created_at do
+    query = '(select created_at from posts where posts.topic_id=topics.id order by created_at desc limit 1)'
+    Arel.sql(query)
+  end
 end
